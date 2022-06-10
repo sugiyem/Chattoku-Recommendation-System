ee5aa2758544247f53b2f6e2b8d3114de758233d
Farrel Dwireswara Salim <farrelds2003@gmail.com>
2022-06-10 09:35:20 +0700

    Change dataset used for the ML model

--- app.py
+++ app.py
@@ -3,29 +3,14 @@ import warnings
 import numpy as np
 import pandas as pd
 from flask import Flask, request, render_template, jsonify
+from model import genre_lists
 
 warnings.simplefilter("ignore", FutureWarning)
 
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
-genre_lists = ['Action', 'Adventure', 'Drama', 'Fantasy', 'Comedy', 'Sci-Fi', 'Suspense',
-               'Romance', 'Slice of Life', 'Supernatural', 'Mystery', 'Award Winning',
-               'Sports', 'Ecchi', 'Avant Garde', 'Horror', 'Boys Love', 'Gourmet',
-               'Girls Love', 'Military', 'Childcare', 'Gore', 'Survival', 'Psychological',
-               'Time Travel', 'Gag Humor', 'Historical', 'Parody', 'Samurai', 'Adult Cast',
-               'Space', 'Romantic Subtext', 'School', 'Iyashikei', 'Strategy Game', 'Mecha',
-               'Super Power', 'Vampire', 'Mythology', 'Team Sports', 'Isekai',
-               'Reincarnation', 'Anthropomorphic', 'Love Polygon', 'Performing Arts',
-               'Combat Sports', 'Martial Arts', 'Delinquents', 'Workplace', 'Music',
-               'Organized Crime', 'Otaku Culture', 'CGDCT', 'Showbiz', 'Detective',
-               'Visual Arts', 'Harem', 'Mahou Shoujo', 'Racing', 'High Stakes Game',
-               'Video Game', 'Idols (Male)', 'Idols (Female)', 'Crossdressing',
-               'Reverse Harem', 'Medical', 'Pets', 'Magical Sex Shift', 'Educational',
-               'Josei', 'Kids', 'Seinen', 'Shoujo', 'Shounen']
-
-anime_data = pd.read_csv("./dataset/mal_top2000_anime.csv",
-                         usecols=["Name", "Score", "Genres", "Theme(s)", "Demographic"])
+anime_data = pd.read_csv("./dataset/mal_top3000.csv")
 
 
 @app.route("/")
@@ -38,22 +23,19 @@ def predict():
     input = request.get_json(force=True)
     genres = input['genres'].split(", ")
 
-    print(genres)
-
     array_genres = pd.Series(0, index=genre_lists)
 
     for genre in set(genres).intersection(genre_lists):
         array_genres.loc[genre] = 1
 
     final_genres = np.array([array_genres.values])
-    print(final_genres)
 
     target_index = model.kneighbors(final_genres, return_distance=False)
-    print(target_index)
+    # print(target_index)
 
     result = anime_data.loc[anime_data.index[target_index][0]]
     json_result = {
-        "result": result.Name.astype(str).values.tolist()
+        "result": result.Mal_id.astype(str).values.tolist()
     }
 
     return jsonify(json_result)
--- model.py
+++ model.py
@@ -1,74 +1,76 @@
+import numpy as np
 import pandas as pd
 import pickle
 from sklearn.neighbors import NearestNeighbors
 
 
-anime_data = pd.read_csv("./dataset/mal_top2000_anime.csv",
-                         usecols=["Name", "Score", "Genres", "Theme(s)", "Demographic"])
+anime_data = pd.read_csv("./dataset/mal_top3000.csv", index_col="Mal_id")
 
-print(anime_data.info())
+# print(anime_data.info())
+# print(anime_data.head())
 
-cleaned_anime_data = anime_data.copy()
-cleaned_anime_data["Theme"] = cleaned_anime_data["Theme(s)"]
+processed_anime_data = anime_data.copy()
 
-genres = cleaned_anime_data.Genres.str.strip(
-    "[]").str.replace("'", "").str.split(", ", expand=True)
+genres = processed_anime_data.Genres.str.strip(
+    "[]").str.replace("'", "").str.split(",", expand=True)
 unique_genres = pd.Series(genres.values.ravel()).dropna().unique()
-unique_genres = unique_genres[unique_genres != 'None']
+unique_genres = unique_genres[unique_genres != ""]
 
-print(len(unique_genres))
-print(unique_genres)
+# print(len(unique_genres))
+# print(unique_genres)
 
 dummy_genres = pd.get_dummies(genres)
 
 for genre in unique_genres:
-    cleaned_anime_data[genre] = dummy_genres.loc[:,
-                                                 dummy_genres.columns.str.endswith(genre)].sum(axis=1)
+    processed_anime_data[genre] = dummy_genres.loc[:,
+                                                   dummy_genres.columns.str.endswith(genre)].sum(axis=1)
 
-print(cleaned_anime_data.head())
+# print(processed_anime_data.head())
 
-themes = cleaned_anime_data.Theme.str.strip(
-    "[]").str.replace("'", "").str.split(", ", expand=True)
+themes = processed_anime_data.Themes.str.strip(
+    "[]").str.replace("'", "").str.split(",", expand=True)
 unique_themes = pd.Series(themes.values.ravel()).dropna().unique()
-unique_themes = unique_themes[unique_themes != 'None']
+unique_themes = unique_themes[unique_themes != ""]
 
-print(len(unique_themes))
-print(unique_themes)
+# print(len(unique_themes))
+# print(unique_themes)
 
 dummy_themes = pd.get_dummies(themes)
 
 for theme in unique_themes:
-    cleaned_anime_data[theme] = dummy_themes.loc[:,
-                                                 dummy_themes.columns.str.endswith(theme)].sum(axis=1)
+    processed_anime_data[theme] = dummy_themes.loc[:,
+                                                   dummy_themes.columns.str.endswith(theme)].sum(axis=1)
 
-print(cleaned_anime_data.head())
+# print(processed_anime_data.head())
 
-demo_dummies = pd.get_dummies(cleaned_anime_data.Demographic)
+demographics = processed_anime_data.Demographics.str.strip(
+    "[]").str.replace("'", "").str.split(",", expand=True)
+unique_demographics = pd.Series(demographics.values.ravel()).dropna().unique()
+unique_demographics = unique_demographics[unique_demographics != ""]
 
-cleaned_anime_data = pd.concat([cleaned_anime_data, demo_dummies], axis=1)
-cleaned_anime_data = cleaned_anime_data.drop(
-    columns=["Name", "Score", "Genres", "Theme(s)", "Demographic", "Theme", "None"])
+# print(len(unique_demographics))
+# print(unique_demographics)
 
-print(cleaned_anime_data.shape[1])
-print(cleaned_anime_data.head())
+dummy_demos = pd.get_dummies(demographics)
+
+for demographic in unique_demographics:
+    processed_anime_data[demographic] = dummy_demos.loc[:,
+                                                        dummy_demos.columns.str.endswith(demographic)].sum(axis=1)
+
+processed_anime_data = processed_anime_data.drop(
+    columns=["Score", "Genres", "Themes", "Demographics"])
+
+# print(processed_anime_data.shape[1])
+# print(processed_anime_data.head())
+
+genre_lists = np.append(unique_genres, np.append(
+    unique_themes, unique_demographics))
+
+# print(len(genre_lists))
+# print(genre_lists)
 
 knn_model = NearestNeighbors(n_neighbors=5)
-knn_model.fit(cleaned_anime_data.values)
-
-
-def get_details(anime_name):
-    return cleaned_anime_data.loc[anime_data[anime_data.Name == anime_name].index].values
-
-
-def get_anime_recommendations_from_name(anime_name):
-    anime_details = get_details(anime_name)
-    target_index = knn_model.kneighbors(anime_details, return_distance=False)
-    print(target_index)
-
-    return anime_data.loc[anime_data.index[target_index][0]]
-
-
-#print(get_anime_recommendations_from_name("Spy x Family"))
+knn_model.fit(processed_anime_data.values)
 
 file = open("model.pkl", "wb")
 pickle.dump(knn_model, file)
